1c2b510ccf7e328ff273a499cc9a2999130a4d42
Nisha S <nishas22ec@psnacet.edu.in>
2026-03-19 15:24:12 +0530

    unauthorize(usercontroller)

--- IdentityServer.Api/Controller/UserController.cs
+++ IdentityServer.Api/Controller/UserController.cs
@@ -10,7 +10,7 @@ namespace IdentityServer.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-[Authorize]
+//[Authorize]
 public class UserController : ControllerBase
 {
     private readonly IMediator _mediator;
